e613c2196e9da4c19db0999196ee76760924d2cb
Gracjan Cisakowski <gracjan.cisakowski@gmail.com>
2017-12-19 11:09:33 +0100

    Create fields for actor model

---
+++ src/main/java/com/codecool/bolec/model/Actor.java
@@ -0,0 +1,13 @@
+package com.codecool.bolec.model;
+
+import java.util.Date;
+
+public class Actor {
+
+    private String name;
+    private String surname;
+    private Date birthdate;
+    private String sex;
+    private String hairColour;
+    private int height;
+}
