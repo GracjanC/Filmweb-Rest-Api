d6777addc46a80e46bf06b53cc1437e782a15129
Gracjan Cisakowski <gracjan.cisakowski@gmail.com>
2017-12-19 14:49:18 +0100

    Add methods from interface implementation with empty bodies

---
+++ src/main/java/com/codecool/bolec/servlets/ActorServlet.java
@@ -0,0 +1,33 @@
+package com.codecool.bolec.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ActorServlet extends HttpServlet implements ServletInterface{
+    @Override
+    public void doPost(HttpServletRequest httpServletRequest,
+                       HttpServletResponse httpServletResponse) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void doPut(HttpServletRequest httpServletRequest,
+                      HttpServletResponse httpServletResponse) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void doDelete(HttpServletRequest httpServletRequest,
+                         HttpServletResponse httpServletResponse) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void doGet(HttpServletRequest httpServletRequest,
+                      HttpServletResponse httpServletResponse) throws ServletException, IOException {
+
+    }
+}
