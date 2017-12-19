3a4e49e9a1db4609cb2023acdefc1dec6942286c
Gracjan Cisakowski <gracjan.cisakowski@gmail.com>
2017-12-19 11:13:46 +0100

    Create getters and setters for actor fields

--- src/main/java/com/codecool/bolec/model/Actor.java
+++ src/main/java/com/codecool/bolec/model/Actor.java
@@ -3,11 +3,67 @@ package com.codecool.bolec.model;
 import java.util.Date;
 
 public class Actor {
-
+    private Long id;
     private String name;
     private String surname;
     private Date birthdate;
     private String sex;
     private String hairColour;
     private int height;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getHairColour() {
+        return hairColour;
+    }
+
+    public void setHairColour(String hairColour) {
+        this.hairColour = hairColour;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
