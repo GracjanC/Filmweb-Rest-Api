3811f817536a56b4a51e5842a07a6e9ccc1f3920
pfolus <kamilnowak91@wp.eu>
2017-12-19 11:56:39 +0100

    Add categories and actors list to movie model

--- src/main/java/com/codecool/bolec/model/Movie.java
+++ src/main/java/com/codecool/bolec/model/Movie.java
@@ -2,10 +2,14 @@ package com.codecool.bolec.model;
 
 import com.codecool.bolec.exceptions.NegativeValueException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Movie {
 
+    private List<Category> categories;
+    private List<Actor> actors;
     private String title;
-    private String category;
     private String director;
     private String review;
     private int minAge;
@@ -16,64 +20,38 @@ public class Movie {
     public Movie() {}
 
 
-    public Movie(String title, String category, String director, String review, int minAge, int rating, int length, int year) {
+    public Movie(String title, String director, String review, int minAge, int rating, int length, int year) {
         this.title = title;
-        this.category = category;
         this.director = director;
         this.review = review;
         this.minAge = minAge;
         this.rating = rating;
         this.length = length;
         this.year = year;
+        this.categories = new ArrayList<>();
+        this.actors = new ArrayList<>();
+
     }
 
-    public String getTitle() {
-        return title;
-    }
+    public String getTitle() { return title; }
 
-    public String getCategory() {
-        return category;
-    }
+    public String getDirector() { return director; }
 
-    public String getDirector() {
-        return director;
-    }
+    public String getReview() { return review; }
 
-    public String getReview() {
-        return review;
-    }
+    public int getMinAge() { return minAge; }
 
-    public int getMinAge() {
-        return minAge;
-    }
+    public int getRating() { return rating; }
 
-    public int getRating() {
-        return rating;
-    }
+    public int getLength() { return length; }
 
-    public int getLength() {
-        return length;
-    }
+    public int getYear() { return year; }
 
-    public int getYear() {
-        return year;
-    }
+    public void setTitle(String title) { this.title = title; }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+    public void setDirector(String director) { this.director = director; }
 
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-    public void setReview(String review) {
-        this.review = review;
-    }
+    public void setReview(String review) { this.review = review; }
 
     public void setMinAge(int minAge) throws NegativeValueException {
 
@@ -105,7 +83,9 @@ public class Movie {
 
     }
 
-    public void setYear(int year) {
-        this.year = year;
-    }
+    public void setYear(int year) { this.year = year; }
+
+    public void addActor(Actor actor) { this.actors.add(actor); }
+
+    public void addCategory(Category category) { this.categories.add(category); }
 }
