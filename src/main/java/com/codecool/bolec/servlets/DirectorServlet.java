e6f31f8b3c18522d5457ae45c825a7f8ac2803b6
Gracjan Cisakowski <gracjan.cisakowski@gmail.com>
2017-12-21 10:47:21 +0100

    Implement doPost method

--- src/main/java/com/codecool/bolec/servlets/DirectorServlet.java
+++ src/main/java/com/codecool/bolec/servlets/DirectorServlet.java
@@ -1,6 +1,7 @@
 package com.codecool.bolec.servlets;
 
 import com.codecool.bolec.model.Director;
+import com.codecool.bolec.services.ServletService;
 import com.codecool.bolec.utils.JSonParser;
 
 import javax.servlet.ServletException;
@@ -9,14 +10,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
+import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns = {"/director/*"})
 public class DirectorServlet extends HttpServlet implements ServletInterface{
+
     @Override
     public void doPost(HttpServletRequest httpServletRequest,
                        HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        String json = httpServletRequest.getReader().lines().collect(Collectors.joining());
 
+        try {
+            JSonParser<Director> jsonParser = new JSonParser<>();
+            ServletService<Director> service = new ServletService<>(Director.class);
+            Director director = jsonParser.jsonToObject(json, Director.class);
+
+            service.post(director);
+            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
