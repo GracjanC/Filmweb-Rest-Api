c9eeb9399808addf32811a1875f0a7364f94070c
wutismyname <plebejoosh@gmail.com>
2017-12-21 09:01:44 +0100

    Adjust code to new generic Service

--- src/main/java/com/codecool/bolec/servlets/CategoryServlet.java
+++ src/main/java/com/codecool/bolec/servlets/CategoryServlet.java
@@ -2,6 +2,7 @@ package com.codecool.bolec.servlets;
 
 import com.codecool.bolec.model.Category;
 import com.codecool.bolec.services.CategoryService;
+import com.codecool.bolec.services.ServletService;
 import com.codecool.bolec.utils.JSonParser;
 
 import javax.persistence.EntityManager;
@@ -25,14 +26,21 @@ public class CategoryServlet extends HttpServlet {
         String idPath = httpServletRequest.getPathInfo();
         String json;
 
-        if (idPath == null) {
-            json = JSonParser.listToJSon(new CategoryService().getAll());
-        } else {
-            Long id = Long.valueOf(idPath.replace("/", ""));
-            json = JSonParser.objectToJSon(new CategoryService().find(id));
-        }
+        try {
+            JSonParser<Category> jsonParser = new JSonParser<>();
+            ServletService<Category> service = new ServletService<>(Category.class);
 
-        httpServletResponse.getWriter().write(json);
+            if (idPath == null) {
+                json = jsonParser.listToJSon(service.getAll());
+            } else {
+                Long id = Long.valueOf(idPath.replace("/", ""));
+                json = jsonParser.objectToJSon(service.getObject(id));
+            }
+
+            httpServletResponse.getWriter().write(json);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -51,7 +59,10 @@ public class CategoryServlet extends HttpServlet {
         EntityManager em = emf.createEntityManager();
 
         try {
-            Category newCategory = (Category) JSonParser.jsonToObject(json, Category.class);
+            JSonParser<Category> jsonParser = new JSonParser<>();
+            ServletService<Category> service = new ServletService<>(Category.class);
+
+            Category newCategory = (Category) jsonParser.jsonToObject(json, Category.class);
             Category oldCategory = em.find(Category.class, Long.valueOf(httpServletRequest.getPathInfo().replace("/","")));
 
             if (oldCategory == null) {
@@ -74,12 +85,18 @@ public class CategoryServlet extends HttpServlet {
 
         String idPath = httpServletRequest.getPathInfo();
 
-        if (idPath == null) {
-            httpServletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
-        } else {
-            Long id = Long.valueOf(idPath.replace("/", ""));
-            new CategoryService().delete(id);
-            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+        try {
+            ServletService<Category> service = new ServletService<>(Category.class);
+
+            if (idPath == null) {
+                httpServletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            } else {
+                Long id = Long.valueOf(idPath.replace("/", ""));
+                service.delete(id);
+                httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
